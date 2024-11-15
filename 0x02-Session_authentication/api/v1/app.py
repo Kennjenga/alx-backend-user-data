5d87e1d3672fa15aafbfa0a77bca72996054812f
Kennjenga <kinyagia10@gmail.com>
2024-11-15 09:42:55 +0300

    Update app.py to support session-based authentication and improve environment variable retrieval

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,7 +5,6 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from api.v1.views import app_views
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -15,12 +14,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = getenv('AUTH_TYPE')
-
+auth_type = os.environ.get('AUTH_TYPE')
 
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
